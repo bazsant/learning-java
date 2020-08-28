284af620273c63a9ec89666070e94b28e95632a1
Jhones Bazilio dos Santos <bazsantsalesforce@gmail.com>
2020-08-27 21:48:08 -0300

    finalizando capitulo 1

---
+++ livro-casa-codigo-desbravando-java/MeuPrimeiroPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrimeiroPrograma {
+	public static void main(String[] args) {
+		System.out.println("O primeiro de muitos!!!");
+		
+		System.out.println("O primeiro de muitos em "+args[0]+" de "+args[1]+" ");
+	}
+}
