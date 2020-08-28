0007d2e6e3a1cad432e6fda7ab9926e1466b38ec
Jhones Bazilio dos Santos <bazsantsalesforce@gmail.com>
2020-08-27 21:26:30 -0300

    aplicativo inicial testando vs code

---
+++ first-hello-world/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        PrintarOlaMundo();
+    }
+
+    private static void PrintarOlaMundo() {
+        System.out.println("Olá mundo!");
+    }
+}
